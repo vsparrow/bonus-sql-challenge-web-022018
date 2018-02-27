fdf9b6868bea3b9bd30d298069b8483d6f42a860
vsparrow <khan@vsparrow.com>
2018-02-27 09:08:52 -0500

    able to add csv rows into db

--- db/seed.rb
+++ db/seed.rb
@@ -19,8 +19,31 @@
 require "sqlite3"
 require 'csv'
 
-#FILE READ COMPLETED
+
+
+
 db = SQLite3::Database.new ":memory:"
+sql = <<-SQL
+  create table guests(
+    year integer,
+    occupation text,
+    show_date datetime,
+    genre text,
+    name texts
+  )
+SQL
+db.execute(sql)
+# puts "#{db.execute('.tables')}"  #not work
+# puts "#{db.execute('.schema')}"  #not work
+# puts "#{db.execute('select * from guests')}"  #currently empty
+# sql = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"  #show tables  ie .tables
+# puts "#{db.execute(sql)}" #[["guests"]]
+
+
 CSV.foreach("./daily_show_guests.csv") do |row|  #./relative to where you run the file. ERRORNO file from ../ but ./ was ok
   puts "#{row}"
+  db.execute "insert into guests values ( ?, ? ,?,?,?)", row[0],row[1],row[2],row[3],row[4]
 end #foreach
+
+puts "#{db.execute('select * from guests Limit 2    ')}"
+# [["YEAR", "GoogleKnowlege_Occupation", "Show", "Group", "Raw_Guest_List"], [1999, "actor", "1/11/99", "Acting", "Michael J. Fox"]]
